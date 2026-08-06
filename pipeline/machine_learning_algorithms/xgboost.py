4d3ed5d0573056450987312a845453b5cb30d090
Yasmim Dantas da Costa Souza <yasmimdantas@MacBook-Air-de-Yasmim.local>
2026-08-05 21:04:18 -0300

    feat: added the gpt2 training methods

--- pipeline/machine_learning_algorithms/xgboost.py
+++ pipeline/machine_learning_algorithms/xgboost.py
@@ -1,7 +1,7 @@
 import xgboost as xgb
 from sklearn.metrics import classification_report, accuracy_score
 import pandas as pd
-
+from pathlib import Path
 
 class XGBoostClassifier:
     def __init__(self, random_state=42, **xgb_kwargs):
@@ -37,6 +37,20 @@ class XGBoostClassifier:
             print(f"Accuracy: {acc:.4f}\n")
             print(classification_report(y_test_df[dim], preds))
         return predictions
+
+    def load_models(self, label, models_dir = 'models/xgboost'):
+
+        models_dir = Path(models_dir)
+
+        for dim in self.dimensions:
+            model_path = models_dir / f"{dim}_{label}.json"
+
+            print(f"Loading {model_path}")
+
+            self.models[dim].load_model(str(model_path))
+
+        print("All models loaded successfully! ✅")
+        return self
 
     def predict(self, X):
         
